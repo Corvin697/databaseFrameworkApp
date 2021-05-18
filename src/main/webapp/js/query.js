83307841fddc5165c998fa49c0a6dbb0c5bb85f8
Jenson <jpc.tank@gmail.com>
2021-05-19 01:27:58 +0200

    Update

--- src/main/webapp/js/query.js
+++ src/main/webapp/js/query.js
@@ -5,8 +5,19 @@ let documentCount = -1
 let bodyElements = ""
 let newInnerHtmlArray = new Array(0)
 const changeEvent = new Event("change")
+let oldTableHtml = ""
 
 document.addEventListener("DOMContentLoaded",(event) => {
+    loadQuery()
+    }, {once:true})
+
+document.addEventListener("change", () => {
+    nextButtonClicked()
+    previousButtonClicked()
+    applyButtonClicked()
+})
+
+function loadQuery() {
     const submitButton = document.getElementById("query-submit-button")
     bodyElements = document.getElementById("body-elements")
     submitButton.addEventListener("click", (event) => {
@@ -25,6 +36,7 @@ document.addEventListener("DOMContentLoaded",(event) => {
                     case "sql":
                         if (bodyElements !== null) {
                             bodyElements.innerHTML = writeSql(asyncRequest)
+                            document.dispatchEvent(changeEvent)
                         }
                         break;
 
@@ -38,28 +50,91 @@ document.addEventListener("DOMContentLoaded",(event) => {
                                 document.dispatchEvent(changeEvent)
                             }
                             break;
+
+                            default:
+                                //Placeholder for Error-Warning
+                                let oldHtml = document.getElementById("body-elements").innerHTML
+                                bodyElements.innerHTML = '<div class="alert alert-danger error-warning" role="alert"> \n' +
+                                    '<h4 class="alert-heading">Error! &#128533 </h4> \n' +
+                                    '<p>Something went wrong</p> \n' +
+                                    '<p> <button class="btn btn-dark btn-block" id="error-ok-button" role ="button">Ok,cool</button> </p> \n' +
+                                    '</div> \n';
+                                let errorOkButton = document.getElementById("error-ok-button")
+                                errorOkButton.addEventListener("click", () => {
+                                    event.preventDefault()
+                                    document.dispatchEvent(changeEvent)
+                                    //Reload query.html
+                                    location.reload()
+                                })
                 }
             }
-            }, )
+        })
+    })
+}
+
+function applyButtonClicked() {
+    const applyButton = document.getElementById("apply-changes-button")
+    if(applyButton !== null) {
+        applyButton.addEventListener("click", (event) => {
+            event.preventDefault()
+
+
+            document.getElementById("table-element").innerHTML =
+                '<div class="alert alert-danger" role="alert"> \n' +
+                '<h4 class="alert-heading">Warning! &#128552 </h4> \n' +
+                ' <p>You´re about to change data directly in the database! This can´t be undone!</p> \n' + '<hr> \n' +
+                '<p class="mb-0"> Are you sure you want to save changes? \n' +
+                '<button class="btn btn-dark btn-block" id="apply-button-yes" role ="button">Yes, apply!</button> \n' +
+                '<button class="btn btn-dark btn-block" id="apply-button-no" role ="button">No, i´m scared</button> \n' +
+                '</p> \n' + '</div> \n';
+
+            warningYesClicked()
+            warningNoClicked()
+        })
+    }
+}
+
+function warningYesClicked() {
+    const yesButton = document.getElementById("apply-button-yes")
+    yesButton.addEventListener("click", (event) => {
+        //TODO: Build Table with changed values
+
+        event.preventDefault()
+
+        let oldHtml = document.getElementById("table-element")
+        oldHtml.innerHTML = '<div class="alert alert-success" role="alert"> \n' +
+            '<h4 class="alert-heading">Success! &#128522</h4> \n' +
+            ' <p> Changes have been applied </p> \n' +
+            '<p> <button class="btn btn-dark btn-block" id="success-ok-button" role ="button">Ok,cool</button> </p> \n' +
+            '</div> \n';
+        let okButton = document.getElementById("success-ok-button")
+        okButton.addEventListener("click", () => {
+            event.preventDefault()
+            //Restore Table
+            //TODO: Show the new, changed table
+            document.getElementById("table-element").innerHTML = oldTableHtml
+        }, {once:true})
     }, {once:true})
 
-}, {once:true})
+}
 
-document.addEventListener("change", () => {
-    nextButtonClicked()
-    previousButtonClicked()
-})
+function warningNoClicked() {
+    const noButton = document.getElementById("apply-button-no")
+    noButton.addEventListener("click", (event) => {
+        event.preventDefault()
+        document.getElementById("table-element").innerHTML = oldTableHtml
+    }, {once:true})
+}
+
 
 function nextButtonClicked() {
     const nextButton = document.getElementById("navigation-button-next")
-    if (nextButton != null) {
+    if (nextButton !== null) {
         nextButton.addEventListener("click", (event) => {
             event.preventDefault()
             //If there´s another document -> Increment documentNumber and show next
             if (documentNumber < documentCount -1) {
                 documentNumber++
-                console.log("Documents:" + documentCount)
-                console.log("Document Number:" + documentNumber)
                 bodyElements.innerHTML = newInnerHtmlArray[documentNumber]
                 //Call change event to signalize that html was changed
                 document.dispatchEvent(changeEvent)
@@ -90,21 +165,18 @@ function writeSql (XMLHttpRequest) {
     let newInnerHtml = ""
     if (XMLHttpRequest != null) {
         const responseText = XMLHttpRequest.responseText;
-        const splittedResponseText = responseText.split(",")
-        const columnCount = parseInt(splittedResponseText [1])
+        const splitResponseText = responseText.split(",")
+        const columnCount = parseInt(splitResponseText [1])
         let firstIndexRowData = 2 + columnCount
-        console.log(firstIndexRowData)
-        console.log(splittedResponseText)
 
-        const rowCount = parseInt(splittedResponseText [splittedResponseText.length - 1])
-        console.log(rowCount)
+        const rowCount = parseInt(splitResponseText [splitResponseText.length - 1])
             newInnerHtml = '<div id="table-element" class="formatted sql-table"> \n' +
                 '<table class="table table-striped table-dark"> \n' +
                 '<thead> \n' + '<tr> \n' + '<th scope="col">#</th> \n';
 
         for (let i = 1; i <= columnCount; i++) {
             //Columnnames are starting at payload postion 2
-            let columnName = splittedResponseText[i + 1]
+            let columnName = splitResponseText[i + 1]
             newInnerHtml = newInnerHtml + '<th scope="col">' + columnName + '</th> \n'
         }
         newInnerHtml = newInnerHtml + '</tr> \n' + '</thead> \n' + '<tbody> \n'
@@ -113,7 +185,7 @@ function writeSql (XMLHttpRequest) {
             newInnerHtml = newInnerHtml + '<tr> \n'
             newInnerHtml = newInnerHtml + '<th scope ="row">' + (i -1) + '</th> \n'
             for (let j = 0; j < columnCount; j++) {
-                let rowData = splittedResponseText[j + firstIndexRowData]
+                let rowData = splitResponseText[j + firstIndexRowData]
                 newInnerHtml = newInnerHtml + '<td>' + rowData + '</td> \n'
             }
             newInnerHtml = newInnerHtml + '</tr> \n'
@@ -128,16 +200,16 @@ function writeMongo(XMLHttpRequest) {
     let newInnerHtmlArray = new Array(0)
     if (XMLHttpRequest != null) {
         const responseText = XMLHttpRequest.responseText;
-        const splittedResponseText = responseText.split(",")
-        const documentCount = parseInt(splittedResponseText[1])
+        const splitResponseText = responseText.split(",")
+        const documentCount = parseInt(splitResponseText[1])
         newInnerHtmlArray = new Array(documentCount)
         let documentLength = 0
         let position = 2
         let i = 0
         for (i; i < documentCount; i++) {
             newInnerHtml ='<div id="document-button" class="formatted"> \n' +
-                '<a href="#" class="btn btn-dark btn-block" id="document-button" role ="button">Show as Document</a> \n' +
-                '</div> \n' + '<div class ="formatted document-count"> \n' + '<h3> Document ' + (i + 1) + ' of ' + documentCount + '</h3> \n </div> \n' +
+                '<button class="btn btn-dark btn-block" id="document-button" role ="button">Show as Document</button> \n' +
+                '</div> \n' + '<div class ="formatted document-count"> \n' + '<h3> Result ' + (i + 1) + ' of ' + documentCount + '</h3> \n </div> \n' +
                 '<div id="table-element" class="formatted document-table"> \n' +
                 '<table class="table table-striped table-dark"> \n' +
                 '<thead> \n' + '<tr> \n' + '<th scope ="col">#</th> \n' +
@@ -147,31 +219,31 @@ function writeMongo(XMLHttpRequest) {
             if (i > 0) {
                 position = position + (2 * documentLength) + 1
             }
-            documentLength = parseInt(splittedResponseText[position])
+            documentLength = parseInt(splitResponseText[position])
             for (let j = 0; j < documentLength; j++) {
 
                 newInnerHtml = newInnerHtml + '<tr> \n' +
                     '<th scope="row">' + j + '</th> \n';
                 //Every entry except _id can be edited
                 if (j === 0) {
-                    newInnerHtml = newInnerHtml + '<td>' + splittedResponseText[position + 1 + j] + '</td> \n' +
-                        '<td>' + splittedResponseText[position + 1 + documentLength + j] + '</td> \n';
+                    newInnerHtml = newInnerHtml + '<td>' + splitResponseText[position + 1 + j] + '</td> \n' +
+                        '<td>' + splitResponseText[position + 1 + documentLength + j] + '</td> \n';
                 } else {
-                    newInnerHtml = newInnerHtml + '<td contenteditable="true">' + splittedResponseText[position + 1 + j] + '</td> \n' +
-                        '<td contenteditable="true">' + splittedResponseText[position + 1 + documentLength + j] + '</td> \n';
+                    newInnerHtml = newInnerHtml + '<td contenteditable="true">' + splitResponseText[position + 1 + j] + '</td> \n' +
+                        '<td contenteditable="true">' + splitResponseText[position + 1 + documentLength + j] + '</td> \n';
                 }
                 newInnerHtml = newInnerHtml + '</tr> \n';
             }
 
             newInnerHtml = newInnerHtml + '</tbody> \n' + '</table> \n' + '</div> \n' +
                 '<div id="apply-button" class="formatted"> \n' +
-                '<a href="#" class="btn btn-dark btn-block" id="apply-changes-button" role ="button">Apply Changes</a> \n' +
+                '<button class="btn btn-dark btn-block" id="apply-changes-button" role ="button">Apply Changes</button> \n' +
                 '</div>';
 
             if (documentCount > 1) {
                 newInnerHtml = newInnerHtml + '<div id="navigation-buttons" class="formatted"> \n' +
-                    '<a href="#" class="btn btn-dark btn-block" id="navigation-button-previous" role="button">Previous Document</a>' +
-                    '<a href="#" class="btn btn-dark btn-block" id="navigation-button-next" role="button">Next Document</a>' +
+                    '<button class="btn btn-dark btn-block" id="navigation-button-previous" role="button">Previous Document</button>' +
+                    '<button class="btn btn-dark btn-block" id="navigation-button-next" role="button">Next Document</button>' +
                     '</div>';
             }
 
